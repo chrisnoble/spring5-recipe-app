bd216df775e3a455573dddc7b98ecc202343def8
Chris Noble <me@chris-noble.uk>
2020-05-12 06:34:49 +0100

    Added display of recipe from index page

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -25,5 +26,16 @@ public class RecipeServiceImpl implements RecipeService{
         recipeRepository.findAll().forEach(recipe -> recipeSet.add(recipe));
 
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long id) {
+        Optional<Recipe> recipeOptonal = recipeRepository.findById(id);
+
+        if (!recipeOptonal.isPresent()) {
+            throw new RuntimeException("Recipe Not Found");
+        }
+
+        return recipeOptonal.get();
     }
 }
